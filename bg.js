f89d5d59237e9e431abd87ff0104b63b824929e0
Alexey Ershov <alex4rom@gmail.com>
2015-05-19 00:06:28 +0200

    Add play sounds function

--- bg.js
+++ bg.js
@@ -22,23 +22,31 @@
         }
     }
 
+    function playSounds (req) {
+        if (req.action == 'rain') {
+            playOrPause(RAIN);
+        }
+
+        if (req.action == 'waves') {
+            playOrPause(WAVES);
+        }
+
+        if (req.action == 'fire') {
+            playOrPause(FIRE);
+        }
+
+        if (req.action == 'forest') {
+            playOrPause(FOREST);
+        } 
+    }
+
+    // chrome.extension.getViews({ type: 'popup' }, function () {
+
+    // });
+
     chrome.runtime.onMessage.addListener(
         function (req, sender, res) {
-            if (req.action == 'rain') {
-                playOrPause(RAIN);
-            }
-
-            if (req.action == 'waves') {
-                playOrPause(WAVES);
-            }
-
-            if (req.action == 'fire') {
-                playOrPause(FIRE);
-            }
-
-            if (req.action == 'forest') {
-                playOrPause(FOREST);
-            }
+            playSounds(req);
         }
     );
 })()
