184b9b7b7749fe80440b1a6195e6953837511658
Alexey Ershov <alex4rom@gmail.com>
2015-05-18 18:43:31 +0300

    Add highlighting

--- popup.js
+++ popup.js
@@ -2,10 +2,25 @@
 
     var btns = document.getElementById('btns');
 
+    function toggleHighlight (el) {
+        // css class that applies highlighting
+        var cls = 'is-active';
+        var hasClass = el.className === cls;
+
+        if (hasClass) {
+            el.className = '';
+        } else {
+            el.className = cls;
+        }
+    }
+
     btns.addEventListener('click', function (e) {
+        var el = e.target;
         var message = e.target.dataset.message;
+
         if (message) {
-            chrome.runtime.sendMessage({ action: message })
+            chrome.runtime.sendMessage({ action: message });
+            toggleHighlight(el);
         }
     })
 
